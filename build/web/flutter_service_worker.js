c242e9b378fc70df830b61614725eda3fc7ce646
adelin232 <narcis.adelin.miulet@gmail.com>
2026-03-15 16:17:59 +0200

    feat: update name

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -24,8 +24,8 @@ const RESOURCES = {"assets/AssetManifest.bin": "2912b6d68039f3351590cfdfa396256f
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "28234b2dd3920d410afa8a7ce35fb077",
-"/": "28234b2dd3920d410afa8a7ce35fb077",
+"index.html": "6bcfdc6ce237fc7c0d49faacc89a6bc6",
+"/": "6bcfdc6ce237fc7c0d49faacc89a6bc6",
 "main.dart.js": "d47485f5864e1d164ac91860d4ca808e",
 "manifest.json": "f4992cfa0b8d23af55ed7abb5821f43d",
 "version.json": "2e3036d3a9aaa3fc7186fc0336cf31e3"};
